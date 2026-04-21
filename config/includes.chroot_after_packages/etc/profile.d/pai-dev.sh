8232c4ce956af1cd00dc8b26f62629da18f56d4c
nirholas <22895867+nirholas@users.noreply.github.com>
2026-04-21 15:05:51 +0000

    🌴 pai-dev.sh

---
+++ config/includes.chroot_after_packages/etc/profile.d/pai-dev.sh
@@ -0,0 +1,23 @@
+# PAI Developer Environment
+export EDITOR="code --wait"
+export VISUAL="code --wait"
+export BROWSER="firefox-esr"
+
+# Colored terminal
+export TERM=xterm-256color
+alias ls='ls --color=auto'
+alias ll='ls -alF'
+alias la='ls -A'
+alias grep='grep --color=auto'
+
+# Git shortcuts
+alias gs='git status'
+alias gc='git commit'
+alias gp='git push'
+alias gl='git log --oneline -20'
+alias gd='git diff'
+
+# Dev shortcuts
+alias py='python3'
+alias serve='python3 -m http.server'
+alias ports='ss -tlnp'
